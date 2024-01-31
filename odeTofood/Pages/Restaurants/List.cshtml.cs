537374a1752ae35eff144f907cf250eb14efbbf8
Eddington <eampofu@gmail.com>
2024-01-31 18:14:53 +0200

    Register data service

--- odeTofood/Pages/Restaurants/List.cshtml.cs
+++ odeTofood/Pages/Restaurants/List.cshtml.cs
@@ -5,17 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using odeTofood.Data;
 
 namespace odeTofood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config,IRestaurantData restaurantData)
         {
             this.config = config;
-            
+            this.restaurantData = restaurantData;
         }
 
        
