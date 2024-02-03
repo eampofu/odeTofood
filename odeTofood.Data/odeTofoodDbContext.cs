76dc35f1eae47c1e20bf326f18886113756602e1
Eddington <eampofu@gmail.com>
2024-02-04 00:19:02 +0200

    Using_the_Entity_Framework_Tools, Adding_Connection_Strings_and_Registering_Services

---
+++ odeTofood.Data/odeTofoodDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using odeTofood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odeTofood.Data
+{
+    public class odeTofoodDbContext:DbContext
+    {
+        public odeTofoodDbContext(DbContextOptions<odeTofoodDbContext>options):base(options) 
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
