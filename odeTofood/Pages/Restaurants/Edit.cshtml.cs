f29335532cebaffcf485926af11f9b38395c2181
Eddington <eampofu@gmail.com>
2024-02-03 14:35:42 +0200

    Creating restaurant page

---
+++ odeTofood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using odeTofood.Core;
+using odeTofood.Data;
+
+namespace odeTofood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        public Restaurant Restaurant { get; set; }
+        public EditModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant==null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
